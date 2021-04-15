450d98fe0c486db83ecf18d743dcf9e979a62252
Patric Forsgard <patric@tasteful.se>
2021-04-15 11:55:34 +0200

    Add test model, migrations and execution

--- src/BugTest/TestClasses.cs
+++ src/BugTest/TestClasses.cs
@@ -1,4 +1,6 @@
-﻿using System.Diagnostics.CodeAnalysis;
+﻿using System.ComponentModel.DataAnnotations;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
 using Microsoft.EntityFrameworkCore;
 
 namespace BugTest
@@ -14,14 +16,34 @@ namespace BugTest
 
         public void Run()
         {
-            
+            _db.TableA.Where(x => !(x.Name == "Adam" && x.Type == "Person")).ToList();
         }
+    }
+
+    public class TableA
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Type { get; set; }
     }
 
     public class Db : DbContext
     {
-        public Db([NotNullAttribute] DbContextOptions options) : base(options)
+        public Db()
         {
         }
+
+        public Db(DbContextOptions options) : base(options)
+        {
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            base.OnConfiguring(optionsBuilder);
+            optionsBuilder.UseSqlServer("Data Source=(local); Integrated Security=True; Initial Catalog=BugTest;MultipleActiveResultSets=True");
+        }
+
+        public DbSet<TableA> TableA { get; set; }
     }
 }
